05e0d58c3f4e26c77168a999629934d9d9534f99
Vincent Battez <vincent.battez.pro@gmail.com>
2020-03-09 22:42:55 +0100

    start front

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Homepage from './pages/homepage/homepage.component';
+import './styles/index.scss';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+    <Homepage />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ frontend/src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.component.scss';
+
+function Homepage() {
+  return (
+    <div className="App">
+      test
+    </div>
+    );
+}
+
+export default Homepage;
