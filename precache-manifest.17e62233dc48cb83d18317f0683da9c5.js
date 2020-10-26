d61c70814d10568261faa8a110a77e39eb41c5d7
Vincent Battez <vincent.battez.pro@gmail.com>
2020-10-26 14:42:27 +0100

    Updates

---
+++ precache-manifest.17e62233dc48cb83d18317f0683da9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d630bb8ace0eaedfff117bc970790c",
+    "url": "/vincentbattez/index.html"
+  },
+  {
+    "revision": "9b2b25ec9685a44d5ffe",
+    "url": "/vincentbattez/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "2cbfa7dce679c520abff",
+    "url": "/vincentbattez/static/css/main.29ff435c.chunk.css"
+  },
+  {
+    "revision": "9b2b25ec9685a44d5ffe",
+    "url": "/vincentbattez/static/js/2.0daa3e27.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vincentbattez/static/js/2.0daa3e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbfa7dce679c520abff",
+    "url": "/vincentbattez/static/js/main.2c5b3947.chunk.js"
+  },
+  {
+    "revision": "1d6e724a3ec5b2562ace",
+    "url": "/vincentbattez/static/js/runtime-main.7f03464e.js"
+  }
+]);
