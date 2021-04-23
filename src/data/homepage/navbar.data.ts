f7c9f9e2aef113059f97d6e0da224e0ef0d95145
Vincent <vincent.battez.pro@gmail.com>
2021-04-23 14:05:40 -0400

    Update navbar.data.ts

--- src/data/homepage/navbar.data.ts
+++ src/data/homepage/navbar.data.ts
@@ -1,6 +1,6 @@
 export const navbarData = {
   link: {
-    label: 'Disponible pour mission freelance',
+    label: 'Disponible pour mandat à partir du 10 mai 2021',
     url: 'https://www.linkedin.com/in/vincentbattez/',
   },
   isAvailable: true
