a6f3e6b7996aee11238d4d8e8c01390d30c28ed0
Vincent <vincent.battez.pro@gmail.com>
2021-07-21 16:07:00 -0400

    Update homepage.data.ts

--- src/data/homepage/homepage.data.ts
+++ src/data/homepage/homepage.data.ts
@@ -1,7 +1,7 @@
 export const homepageData = {
   subTitle: 'Porfolio Développeur Front-End Confirmé',
   title: 'Développeur Front-End Confirmé affinité UX/UI',
-  description: 'Disponible pour un mandat à Montréal.',
+  description: 'Recherche un développeur front-end pour travailler à Montréal.',
   image: {
     url: require('../../assets/face-vector.svg')
   },
